d7e93e4d1d69d851507e656bb561325a5638240e
André Caldas <andre.em.caldas@gmail.com>
2021-10-28 12:29:48 -0300

    Remove TABs.

--- audio_aligner/play.cpp
+++ audio_aligner/play.cpp
@@ -10,31 +10,31 @@ int lags[] = {10, -10, 50, 51, 52, 53, -9, -11, -12, -50, 100, -90, 150};
 
 void play(const char *filename)
 {
-	Profile profile; // defaults to dv_pal
-	for(int lag: lags)
-	{
-		Producer producer_x(profile, filename);
-		Producer producer_y(profile, filename);
+..Profile profile; // defaults to dv_pal
+..for(int lag: lags)
+..{
+....Producer producer_x(profile, filename);
+....Producer producer_y(profile, filename);
 
-		std::cout << "Lag: " << lag << " frames." << std::endl;
+....std::cout << "Lag: " << lag << " frames." << std::endl;
 
-		producer_x.set_in_and_out(200, 2246);
-		producer_y.set_in_and_out(200+lag, 2000+lag);
+....producer_x.set_in_and_out(200, 2246);
+....producer_y.set_in_and_out(200+lag, 2000+lag);
 
-		int max = std::max(producer_x.get_playtime(), producer_y.get_playtime());
+....int max = std::max(producer_x.get_playtime(), producer_y.get_playtime());
 
-		AudioEnvelopeFFT envelope_x(producer_x, max);
-		AudioEnvelopeFFT envelope_y(producer_y, max);
+....AudioEnvelopeFFT envelope_x(producer_x, max);
+....AudioEnvelopeFFT envelope_y(producer_y, max);
 
-		int calc_lag = envelope_x.get_lag_from(envelope_y);
-		std::cout << "Calculated lag: " << calc_lag << std::endl;
-	}
+....int calc_lag = envelope_x.get_lag_from(envelope_y);
+....std::cout << "Calculated lag: " << calc_lag << std::endl;
+..}
 }
 
 int main( int, char **argv )
 {
-	Factory::init();
-	play(argv[1]);
-	Factory::close();
-	return 0;
+..Factory::init();
+..play(argv[1]);
+..Factory::close();
+..return 0;
 }
