afe82dda43dcfd2be5bf22d662f3bf733cbd5108
André Caldas <andre.em.caldas@gmail.com>
2021-10-28 21:15:23 -0300

    Mathematical and emphirical adjustments.
    Uses absolute value for envelope.
    Removes mean value.
    Takes a small sample for each frame.

--- audio_aligner/AudioEnvelopeFFT.cpp
+++ audio_aligner/AudioEnvelopeFFT.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <iostream>
 #include <memory>
 
 #include "AudioEnvelopeFFT.h"
@@ -20,10 +22,12 @@ AudioEnvelopeFFT::AudioEnvelopeFFT(Mlt::Producer& producer, size_t n_frames)
 
     init(n_frames);
 
-    int frequency = 1600;
+    std::cerr << "Extracting audio...";
+    int frequency = 5 * producer.get_fps();
     int channels = 1;
     mlt_audio_format format = mlt_audio_s16;
 
+    double total = 0;
     for (size_t i = 0; i < play_frames; ++i)
     {
         std::unique_ptr<Mlt::Frame> frame(producer.get_frame(i));
@@ -31,10 +35,23 @@ AudioEnvelopeFFT::AudioEnvelopeFFT(Mlt::Producer& producer, size_t n_frames)
         int samples = mlt_audio_calculate_frame_samples(float(producer.get_fps()), frequency, position);
         auto* data = static_cast<int16_t*>(frame->get_audio(format, frequency, channels, samples));
 
-        envelope(i) = 0; // Redundant.
-        for (int k = 0; k < samples; ++k) {
-            envelope(i) += data[k];
+        double samples_value = 0;
+        for(int k = 0; k < samples; ++k)
+        {
+            samples_value += std::abs(data[k]);
         }
+        samples_value = samples_value / samples;
+        envelope(i) = samples_value;
+        total += samples_value;
     }
+
+    double mean = total / play_frames;
+    std::cerr << "Mean: " << mean << "." << std::endl;
+    for (size_t i = 0; i < play_frames; ++i)
+    {
+        envelope(i) = envelope(i) - mean;
+    }
+
+    std::cerr << " Done." << std::endl;
 }
 
