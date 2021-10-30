ffbc87ae20aa68cb582f5ca8ef17b10a3f49c9ae
André Caldas <andre.em.caldas@gmail.com>
2021-10-30 11:32:26 -0300

    Determine drift AND lag.

--- audio_aligner/drift.cpp
+++ audio_aligner/drift.cpp
@@ -21,16 +21,14 @@ void usage (int return_value = 0)
 }
 
 
-double drift_score (FFTInplaceArray& x, FFTInplaceArray& y, double drift)
+void drift_score_and_lag (FFTInplaceArray& x, FFTInplaceArray& y, double drift, double* score, int* lag)
 {
   FFTInplaceArray drifted_y = y.clone(drift);
-  double quality = 0.0;
-  x.get_lag_from(drifted_y, &quality);
-  return quality;
+  *lag = x.get_lag_from(drifted_y, score);
 }
 
 
-void get_drift_sub(FFTInplaceArray& x, FFTInplaceArray& y, double drift_range, double drift_about, int precision)
+void get_drift_and_lag_sub(FFTInplaceArray& x, FFTInplaceArray& y, double drift_range, double* drift_about, int precision, int* lag)
 {
   if(precision >= 2)
   {
@@ -39,32 +37,29 @@ void get_drift_sub(FFTInplaceArray& x, FFTInplaceArray& y, double drift_range, d
   double drift_step = std::pow(10.0, -precision);
 
   std::cout << "Precision: " << drift_step * 100 << "%." << std::endl;
-  std::cout << "Range: (" << (drift_about-drift_range) * 100
-            << "%, " << (drift_about+drift_range) * 100 << "%)." << std::endl;
+  std::cout << "Range: (" << (*drift_about-drift_range) * 100
+            << "%, " << (*drift_about+drift_range) * 100 << "%)." << std::endl;
 
-  double max_score = drift_about;
+  double max_score = *drift_about;
   double best_drift = 1.0;
-  for(double i = drift_about-drift_range; i <= drift_about+drift_range; i += drift_step)
+  for(double i = *drift_about-drift_range; i <= *drift_about+drift_range; i += drift_step)
   {
-    double score = drift_score(x, y, i);
+    double score = 0.0;
+    drift_score_and_lag(x, y, i, &score, lag);
     if(score > max_score)
     {
       max_score = score;
       best_drift = i;
     }
   }
+  *drift_about = best_drift;
 
 
   if(precision < 9)
   {
-    std::cout << "Drift up to now: " << best_drift * 100 << "%." << std::endl;
-    get_drift_sub(x, y, drift_range/10, best_drift, precision+1);
-  }
-  else
-  {
-    std::cout << "Adjust the speed of the second clip"
-              << " to " << best_drift * 100 << "%." << std::endl;
-
+    std::cout << "Drift up to now: " << best_drift * 100 << "%."
+              << " Lag: " << *lag << " frames." << std::endl;
+    get_drift_and_lag_sub(x, y, drift_range/10, drift_about, precision+1, lag);
   }
 }
 
@@ -92,7 +87,34 @@ void get_drift(Profile& profile, const char *f1, const char *f2, double drift_ra
 
   std::cout << "Calculating speed change for the file " << f2 << "." << std::endl;
 
-  get_drift_sub(envelope_x, envelope_y, drift_range, drift_about, precision);
+  int lag = 0;
+  get_drift_and_lag_sub(envelope_x, envelope_y, drift_range, &drift_about, precision, &lag);
+
+
+  std::cout << "Adjust the speed of the second clip"
+            << " to " << drift_about * 100 << "%." << std::endl;
+
+  std::cout << "AFTER adjusting the drift, adjust clip relative position." << std::endl;
+
+  char sign = '+';
+  if(lag < 0)
+  {
+    sign = '-';
+    lag *= -1;
+  }
+
+  float fps = profile.fps();
+  int hours = std::floor(lag/(60*60 * fps));
+  int minutes = std::floor(lag/(60 * fps)) - 60*hours;
+  int seconds = std::floor(lag/(fps)) - 60*60*hours - 60*minutes;
+  int frames = std::floor(lag) - 60*60*fps*hours - 60*fps*minutes - fps*seconds;
+
+  std::cout << "Calculated lag: " << sign << lag << " frames. " << ((float)lag) / fps << " seconds." << std::endl;
+
+  std::cout << sign << hours << "h"
+            << minutes << "m"
+            << seconds << "s (" << sign
+            << frames << " frames) (fps = " << fps << ")." << std::endl;
 }
 
 
