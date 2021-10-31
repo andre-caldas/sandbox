1ccf103957ce99f3ca4df689ddeb36972fce4fc0
André Caldas <andre.em.caldas@gmail.com>
2021-10-30 23:57:46 -0300

    Use boost program_options.

--- audio_aligner/drift.cpp
+++ audio_aligner/drift.cpp
@@ -1,6 +1,10 @@
+#include <boost/program_options.hpp>
+
+#include <cstdlib>
 #include <chrono>
 #include <thread>
 #include <string>
+#include <vector>
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -10,16 +14,90 @@
 #include "AudioEnvelopeFFT.h"
 
 
-using namespace Mlt;
+static bool use_percent;
+static bool debug;
+static bool script_output;
+static std::string profile_name;
+static std::string reference_file;
+static std::vector<std::string> input_files;
+static double approximate_drift;
+static double drift_range;
+static int final_precision;
 
-void usage (int return_value = 0)
+
+namespace po = boost::program_options;
+
+po::variables_map process_args(int argc, const char* argv[])
 {
-  auto& stream = (0 == return_value) ? std::cout : std::cerr;
-  stream << "Usage:" << std::endl;
-  stream << "\tdrift <file_1> <file_2> [<drift_range> [<profile> [...]]]." << std::endl;
-  stream << "Look for speed correction for <file_2> between (1.0 - <drift_range>) and (1.0 + <drift_range>)." << std::endl;
+  po::options_description desc("Options");
+  desc.add_options()
+    ("help,h",
+        "help message")
+    ("script,s",
+        po::value<bool>(&script_output)->default_value(false),
+        "output one line with <un-drift speed> <lag direction> <lag frame count>")
+    ("profile,p",
+        po::value<std::string>(&profile_name)->default_value("atsc_1080p_60"),
+        "MLT profile")
+    ("report-percent",
+        po::value<bool>(&use_percent)->default_value(false),
+        "use percentage (i.e.: %) in reports")
+    ("debug,d",
+        po::value<bool>(&debug)->default_value(false),
+        "print debug output")
+    ("approximate-drift,a",
+        po::value<double>(&approximate_drift)->default_value(1.0),
+        "initial drift estimation")
+    ("drift-range,r",
+        po::value<double>(&drift_range)->default_value(0.01),
+        "initial drift estimation")
+    ("precision,m",
+        po::value<int>(&final_precision)->default_value(9),
+        "number of precision digits")
+    ("base-file,b",
+        po::value<std::string>(&reference_file)->required(),
+        "reference to align to")
+    ("input-file,i",
+        po::value<std::vector<std::string>>(&input_files)->required(),
+        "file to be un-drifted and aligned")
+  ;
+
+  po::positional_options_description p;
+  po::variables_map vm;
+
+  try
+  {
+    p.add("base-file", 1);
+    p.add("input-file", 1);
+    p.add("input-file", -1);
+
+    po::store(po::command_line_parser(argc, argv).
+      options(desc).positional(p).run(), vm);
+
+    if(vm.count("help"))
+    {
+      std::cerr << desc << std::endl;
+      std::exit(0);
+    }
+
+    po::notify(vm);
+  }
+  catch(std::exception& e)
+  {
+      std::cerr << "Error: " << e.what() << std::endl;
+      std::exit(1);
+  }
+  catch(...)
+  {
+      std::cerr << "Unknown error!" << std::endl;
+      std::exit(2);
+  }
+
+  return vm;
 }
 
+
+using namespace Mlt;
 
 void drift_score_and_lag (FFTInplaceArray& x, FFTInplaceArray& y, double drift, double* score, int* lag)
 {
@@ -55,7 +133,7 @@ void get_drift_and_lag_sub(FFTInplaceArray& x, FFTInplaceArray& y, double drift_
   *drift_about = best_drift;
 
 
-  if(precision < 9)
+  if(precision < final_precision)
   {
     std::cout << "Drift up to now: " << best_drift * 100 << "%."
               << " Lag: " << *lag << " frames." << std::endl;
@@ -64,7 +142,7 @@ void get_drift_and_lag_sub(FFTInplaceArray& x, FFTInplaceArray& y, double drift_
 }
 
 
-void get_drift(Profile& profile, const char *f1, const char *f2, double drift_range, double drift_about = 1.0, int precision = 4)
+void get_drift(Profile& profile, const char *f1, const char *f2, double drift_range, double drift_about, int precision = 4)
 {
   Producer producer_x(profile, f1);
   Producer producer_y(profile, f2);
@@ -120,34 +198,17 @@ void get_drift(Profile& profile, const char *f1, const char *f2, double drift_ra
 
 int main( int argc, const char *argv[] )
 {
-  if(argc <= 2)
-  {
-      usage(1);
-      return 1;
-  }
-
-  double drift_range = 0.01; // 99% to 101%.
-  if(argc > 3)
-  {
-    drift_range = std::stof(argv[3]);
-  }
+  auto vm = process_args(argc, argv);
 
   Factory::init();
-  if(argc > 4)
+
+  Profile profile(profile_name.c_str());
+  for(auto& drifted_file: input_files)
   {
-    for(int i = 4; i < argc; ++i)
-    {
-      std::cout << "/=== PROFILE: " << argv[i] << " ===" << std::endl;
-      Profile profile(argv[i]);
-      get_drift(profile, argv[1], argv[2], drift_range);
-    }
+    std::cout << "/=== PROFILE: " << profile_name << " ===" << std::endl;
+    get_drift(profile, reference_file.c_str(), drifted_file.c_str(), drift_range, approximate_drift);
   }
-  else
-  {
-    std::cout << "/=== PROFILE: " << "atsc_1080p_60" << " ===" << std::endl;
-    Profile profile("atsc_1080p_60");
-    get_drift(profile, argv[1], argv[2], drift_range);
-  }
+
   Factory::close();
   return 0;
 }
