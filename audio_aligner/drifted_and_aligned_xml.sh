0a2998d2d7651db5f1cb58df0ab74c3dec6c3dda
André Caldas <andre.em.caldas@gmail.com>
2021-11-01 10:29:17 -0300

    Uses `drift`'s command line arguments.

--- audio_aligner/drifted_and_aligned_xml.sh
+++ audio_aligner/drifted_and_aligned_xml.sh
@@ -4,28 +4,17 @@ set -x
 BINPATH="$(dirname "$0")"
 
 file1="$1"
-shift
-file2="$1"
-shift
+file2="$2"
+profile="${3:-atsc_1080p_60}"
 
-speed_and_lag="$("$BINPATH/drift" "$file1" "$file2" 0.01 "atsc_1080p_60" |
-	egrep '(Adjust the speed)|(Calculated lag)' |
-	sed -r -e 's,.*Adjust the speed.* ([0-9.]*%).*,\1,' \
-	-e 's,Calculated lag: ([-+])([0-9]*) .*,\1\n\2,')"
+speed_and_lag="$("$BINPATH/drift" --script "$file1" "$file2" --profile "$profile" --print-precision 8)"
 
-speed="$(echo "$speed_and_lag" | cut -d "
-" -f 1)"
-lag_sign="$(echo "$speed_and_lag" | cut -d "
-" -f 2)"
-lag="$(echo "$speed_and_lag" | cut -d "
-" -f 3)"
+speed="$(echo "$speed_and_lag" | cut -d ";" -f 1)"
+lag_file="$(echo "$speed_and_lag" | cut -d ";" -f 2)"
+lag="$(echo "$speed_and_lag" | cut -d ";" -f 3)"
 
 blank1=""
 blank2=""
-if [ '-' == "$lag_sign" ]; then
-	blank1="-blank $lag";
-else
-	blank2="-blank $lag";
-fi
+declare "blank$lag_file"="-blank $lag"
 
-melt -profile "atsc_1080p_60" $blank1 "$file1" -track $blank2 "$file2" -consumer xml
+melt -profile "$profile" -video-track $blank1 "$file1" -audio-track $blank2 timewarp:"$speed":"$file2" -consumer xml
