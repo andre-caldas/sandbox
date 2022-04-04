c1a778b8228225b2daea049f1f11142e5b16bf55
André Caldas <andre.em.caldas@gmail.com>
2022-04-04 17:49:16 -0300

    Use miliseconds instead of frames.

--- audio_aligner/AudioEnvelopeFFT.cpp
+++ audio_aligner/AudioEnvelopeFFT.cpp
@@ -20,36 +20,55 @@ AudioEnvelopeFFT::AudioEnvelopeFFT(Mlt::Producer& producer, size_t n_frames)
     size_t play_frames = producer.get_playtime();
     play_frames = std::min(n_frames, play_frames);
 
-    init(n_frames);
+    const int fraction_of_a_second = 240;
+    init(std::ceil(double(fraction_of_a_second * n_frames) / producer.get_fps()));
 
     std::cerr << "Extracting audio...";
+
+    int frequency = 48000;
+    // I don't really know how to properly handle "channels".
+    // So, let's use channels = 1.
+    int channels = 1;
+    mlt_audio_format format = mlt_audio_s16;
+
+    // I hope frame->get_audio() does not change the value of "frequency".
+    // I believe the correct thing to do would probably be:
+    // - Consider a different frequency for each frame!
+    // But we are assuming the frequency is equal to 48000 for every frame.
+    // A better approach would be to assume the frequency is constant,
+    // but not necessarily equal to 48000. But then, if it is not a multiple
+    // of "fraction_of_a_second", we would have a problem! :-(
+    const int samples_per_fraction = frequency / fraction_of_a_second;
+    int current_sample_set = 0;
+    int samples_to_add = samples_per_fraction;
+    assert(samples_to_add > 0);
+
     double total = 0;
     for (size_t i = 0; i < play_frames; ++i)
     {
-        // Unfortunately, we have to put this inside the loop,
-        // because MLT uses them to pass parameters.
-        int frequency = 48000;
-        int channels = 2;
-        mlt_audio_format format = mlt_audio_s16;
-
         std::unique_ptr<Mlt::Frame> frame(producer.get_frame(i));
         mlt_position position = mlt_frame_get_position(frame->get_frame());
         int samples = mlt_audio_calculate_frame_samples(float(producer.get_fps()), frequency, position);
         auto* data = static_cast<int16_t*>(frame->get_audio(format, frequency, channels, samples));
+        assert(48000 == frequency);
 
-        double samples_value = 0;
-        for(int k = 0; k < samples * channels; ++k)
+        for(int k = 0; k < samples; ++k)
         {
-            samples_value += std::abs(data[k]);
+            if (samples_to_add <= 0)
+            {
+              ++current_sample_set;
+              envelope(current_sample_set) = 0;
+              samples_to_add = samples_per_fraction;
+            }
+            envelope(current_sample_set) += std::abs(data[k]);
+            total += std::abs(data[k]);
+            --samples_to_add;
         }
-        samples_value = samples_value / samples;
-        envelope(i) = samples_value;
-        total += samples_value;
     }
 
-    double mean = total / play_frames;
+    double mean = total / (current_sample_set + 1);
     std::cerr << "Mean: " << mean << "." << std::endl;
-    for (size_t i = 0; i < play_frames; ++i)
+    for (int i = 0; i <= current_sample_set; ++i)
     {
         envelope(i) = envelope(i) - mean;
     }
